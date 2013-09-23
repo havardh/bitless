fed6f528977f2a40b76c89a7bf6c94c35b8aaf32
Kristian Klomsten Skordal <kristian.skordal@gmail.com>
2013-09-23 14:20:50 +0200

    Add handling of all flags to the adder

--- vhdl/pipeline/core/alu/adder.vhd
+++ vhdl/pipeline/core/alu/adder.vhd
@@ -1,6 +1,7 @@
 -- Full 16-bit adder module, yay!
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 library work;
 use work.core_constants.all;
@@ -34,9 +35,11 @@ architecture behaviour of adder is
 
 	signal propagates : std_logic_vector(3 downto 0) := (others => '0');
 	signal generates : std_logic_vector(3 downto 0) := (others => '0');
-	signal carries : std_logic_vector(3 downto 0) := (others => '0');
+	signal carries : std_logic_vector(4 downto 0) := (others => '0');
+	signal result_register : std_logic_vector(15 downto 0);
 
 begin
+	result <= result_register;
 	carries(0) <= '0';
 
 	lcu: lookahead_carry_unit
@@ -49,11 +52,11 @@ begin
 			g4 => generates(1),
 			g8 => generates(2),
 			g12 => generates(3),
-			c0 => '0',
+			c0 => carries(0),
 			c4 => carries(1),
 			c8 => carries(2),
 			c12 => carries(3),
-			c16 => flags.carry
+			c16 => carries(4)
 		);
 
 	generate_adders:
@@ -62,13 +65,30 @@ begin
 			port map(
 				a => a(((i + 1) * 4) - 1 downto i * 4),
 				b => b(((i + 1) * 4) - 1 downto i * 4),
-				result => result(((i + 1) * 4) - 1 downto i * 4),
+				result => result_register(((i + 1) * 4) - 1 downto i * 4),
 				c => carries(i),
 				p => propagates(i),
 				g => generates(i)
 			);
 	end generate;
-			
+
+	update_flags_process: process(result_register)
+	begin
+		flags.negative <= '0';
+		flags.overflow <= '0';
+		flags.zero <= '0';
+		flags.carry <= carries(4);
+
+		if result_register = x"0000" then
+			flags.zero <= '1';
+		end if;
+		if result_register(15) = '1' then
+			flags.negative <= '1';
+		end if;
+		if carries(4) = '1' then
+			flags.overflow <= '1';
+		end if;
+	end process;
 
 end behaviour;
 
