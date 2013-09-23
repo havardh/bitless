3ed4914fbbee385375c151099508246e4740befa
Kristian Klomsten Skordal <kristian.skordal@gmail.com>
2013-09-23 10:11:14 +0200

    Add EBI header that somehow became empty yesterday
    Stupid ISE, removing my code :-(

--- vhdl/sw/ebi_interface.h
+++ vhdl/sw/ebi_interface.h
@@ -3,6 +3,19 @@
 #ifndef FPGA_EBI_INTERFACE_H
 #define FPGA_EBI_INTERFACE_H
 
+// Constructs an address for indexing a specific module in the FPGA.
+#define FPGA_EBI_ADDRESS(pipeline, device, address)
+	((pipeline & 3) << 26 | (device & 0x1f) << 21 | (address & 0xfffff))
 
+// Device number for accessing the control register of a pipeline:
+#define FPGA_PIPELINE_CONTROL	0
+// Device number for accessing the input buffer of a pipeline:
+#define FPGA_PIPELINE_INPUT	1
+// Device number for accessing the output buffer of a pipeline:
+#define FPGA_PIPELINE_OUTPUT	2
+// Device number for accessing the constant register of a pipeline:
+#define FPGA_PIPELINE_CONSTANT	3
+// Device number for accessing a core in a pipeline (0 indexed):
+#define FPGA_PIPELINE_CORE(x)	(x + 4)
 
 #endif
