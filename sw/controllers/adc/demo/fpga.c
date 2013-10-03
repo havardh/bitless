7d02bb645a85d35cfc8a4f5fbef3488ddf018023
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-10-03 13:45:40 +0200

    Refactored DMA Driver to rely on interface of FPGA and not internals.

--- sw/controllers/adc/demo/fpga.c
+++ sw/controllers/adc/demo/fpga.c
@@ -1,24 +1,54 @@
 #include "fpga.h"
 
-extern volatile bool preampProcessPrimary;
+static bool bufferPrimary;
 
-uint16_t *preampAudioInBuffer1;
-uint16_t *preampAudioInBuffer2;
-uint32_t *preampAudioOutBuffer1;
-uint32_t *preampAudioOutBuffer2;
+uint16_t *primaryAudioInBuffer;
+uint16_t *secondaryAudioInBuffer;
+uint32_t *primaryAudioOutBuffer;
+uint32_t *secondaryAudioOutBuffer;
 
 void FPGA_Init( void ) 
-{ /*
-	preampAudioInBuffer1 = (uint16_t*)malloc(sizeof(uint16_t) * BUFFER_SIZE * 2);
-	preampAudioInBuffer2 = (uint16_t*)malloc(sizeof(uint16_t) * BUFFER_SIZE * 2);
-	preampAudioOutBuffer1 = (uint16_t*)malloc(sizeof(uint32_t) * BUFFER_SIZE);
-	preampAudioOutBuffer2 = (uint16_t*)malloc(sizeof(uint32_t) * BUFFER_SIZE);
-	*/
-	preampAudioInBuffer1 = EXT_SRAM_BASE_ADDRESS + ((sizeof(uint32_t) * BUFFER_SIZE) * 0);
-	preampAudioInBuffer2 = EXT_SRAM_BASE_ADDRESS + ((sizeof(uint32_t) * BUFFER_SIZE) * 1);
-	preampAudioOutBuffer1 = EXT_SRAM_BASE_ADDRESS + ((sizeof(uint32_t) * BUFFER_SIZE)* 2);
-	preampAudioOutBuffer2 = EXT_SRAM_BASE_ADDRESS + ((sizeof(uint32_t) * BUFFER_SIZE)* 3);
+{
+	// Allocate Space in SRAM
+  primaryAudioInBuffer    = EXT_SRAM_BASE_ADDRESS + ((sizeof(uint32_t) * BUFFER_SIZE) * 0);
+	secondaryAudioInBuffer  = EXT_SRAM_BASE_ADDRESS + ((sizeof(uint32_t) * BUFFER_SIZE) * 1);
+  primaryAudioOutBuffer   = EXT_SRAM_BASE_ADDRESS + ((sizeof(uint32_t) * BUFFER_SIZE) * 2);
+  secondaryAudioOutBuffer = EXT_SRAM_BASE_ADDRESS + ((sizeof(uint32_t) * BUFFER_SIZE) * 3);
+}
 
+uint16_t* FPGA_GetPrimaryAudioInBuffer( void ) 
+{
+	return primaryAudioInBuffer;
+}
+
+uint16_t* FPGA_GetSecondaryAudioInBuffer( void )
+{
+	return secondaryAudioInBuffer;
+}
+
+int FPGA_GetAudioInBufferSize( void ) 
+{
+	return BUFFER_SIZE * 2;
+}
+
+uint32_t* FPGA_GetPrimaryAudioOutBuffer( void )
+{
+	return primaryAudioOutBuffer;
+}
+
+uint32_t* FPGA_GetSecondaryAudioOutBuffer( void ) 
+{
+	return secondaryAudioOutBuffer;
+}
+
+int FPGA_GetAudioOutBufferSize( void ) 
+{
+	return BUFFER_SIZE;
+}
+
+void FPGA_SetBufferPrimary( bool primary ) 
+{
+	bufferPrimary = primary;
 }
 
 void PendSV_Handler(void)
@@ -29,15 +59,15 @@ void PendSV_Handler(void)
   int32_t right;
   int32_t left;
 
-	if (preampProcessPrimary)
+	if (bufferPrimary)
 		{
-			inBuf  = preampAudioInBuffer1;
-			outBuf = preampAudioOutBuffer1;
+			inBuf  = primaryAudioInBuffer;
+			outBuf = primaryAudioOutBuffer;
 		}
   else
 		{
-			inBuf  = preampAudioInBuffer2;
-			outBuf = preampAudioOutBuffer2;
+			inBuf  = secondaryAudioInBuffer;
+			outBuf = secondaryAudioOutBuffer;
 		}
 
 	int i=0; 
--- sw/controllers/adc/demo/fpga.h
+++ sw/controllers/adc/demo/fpga.h
@@ -5,14 +5,23 @@
 #include <stdbool.h>
 
 #define BUFFER_SIZE     64     /* 64/44100 = appr 1.5 msec delay */
+
+// Start address of SRAM on DK3750 
 #define EXT_SRAM_BASE_ADDRESS ((volatile uint16_t*) 0x88000000)
 
-extern uint16_t *preampAudioInBuffer1;
-extern uint16_t *preampAudioInBuffer2;
-extern uint32_t *preampAudioOutBuffer1;
-extern uint32_t *preampAudioOutBuffer2;
 
 void FPGA_Init( void );
+
+uint16_t* FPGA_GetPrimaryAudioInBuffer( void );
+uint16_t* FPGA_GetSecondaryAudioInBuffer( void );
+int FPGA_GetAudioInBufferSize( void );
+
+uint32_t* FPGA_GetPrimaryAudioOutBuffer( void );
+uint32_t* FPGA_GetSecondaryAudioOutBuffer( void );
+int FPGA_GetAudioOutBufferSize( void );
+
+void FPGA_SetBufferPrimary( bool primary );
+
 
 #endif /* _FPGA_H_ */
 
