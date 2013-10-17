6e1b1bef201c37d39ad37afae03a996e86ed6bac
Sondre Lefsaker <sondrele@stud.ntnu.no>
2013-10-17 17:03:41 +0200

    refactoring

--- sw/controllers/fpga/inc/FPGAController.h
+++ sw/controllers/fpga/inc/FPGAController.h
@@ -26,12 +26,6 @@ typedef struct {
     FPGA_Pipeline *pipelines;
 } FPGA_Processor;
 
-/* Setup and teardown */
-void FPGA_Init(FPGAConfig *config);
-void FPGA_Destroy();
-void FPGA_Pipeline_New(FPGA_Pipeline *pipeline, uint32_t pipelinePos, FPGAConfig *config);
-void FPGA_Core_New(FPGA_Core *core, uint32_t corePos, uint32_t pipelinePos, FPGAConfig *config);
-
 /* FPGA Processor methods */
 FPGA_Pipeline* FPGA_GetPipeline(uint32_t pipeline);
 FPGA_Core* FPGA_GetCore(uint32_t pipeline, uint32_t core);
@@ -45,5 +39,11 @@ FPGA_Core* FPGAPipline_GetCore(FPGA_Pipeline *pipeline, uint32_t core);
 /* FPGA Core methods */
 void FPGACore_GetProgram(FPGA_Core *core, uint16_t *program);
 void FPGACore_SetProgram(FPGA_Core *core, uint16_t *program, uint32_t programSize);
+
+/* Setup and teardown */
+void FPGA_Init(FPGAConfig *config);
+void FPGA_Destroy(void);
+void FPGA_Pipeline_New(FPGA_Pipeline *pipeline, uint32_t pipelinePos, FPGAConfig *config);
+void FPGA_Core_New(FPGA_Core *core, uint32_t corePos, uint32_t pipelinePos, FPGAConfig *config);
 
 #endif /* _FPGACONTROLLER_H_ */
--- sw/controllers/fpga/src/FPGAController.c
+++ sw/controllers/fpga/src/FPGAController.c
@@ -4,13 +4,80 @@
 
 static FPGA_Processor proc;
 
+/*******************************************
+* FPGA Processor methods                   *
+*******************************************/
+
+uint16_t* FPGA_GetBaseAddress(void) {
+    return proc.baseAddress;
+}
+
+FPGA_Pipeline* FPGA_GetPipeline(uint32_t pipeline) {
+    if (pipeline < proc.numPipelines) {
+        return (proc.pipelines + pipeline);
+    }
+    return NULL;
+}
+
+FPGA_Core* FPGA_GetCore(uint32_t pipeline, uint32_t core) {
+    FPGA_Pipeline *p = FPGA_GetPipeline(pipeline);
+    return FPGAPipline_GetCore(p, core);
+}
+
+/*******************************************
+* FPGA Pipeline methods                    *
+*******************************************/
+
+FPGA_Core* FPGAPipline_GetCore(FPGA_Pipeline *pipeline, uint32_t core) {
+    if (pipeline != NULL && core < pipeline->numCores) {
+        return (pipeline->cores + core);
+    }
+    return NULL;
+}
+
+uint16_t* FPGAPipeline_GetInputBuffer(FPGA_Pipeline *pipeline) {
+    FPGA_Core *core = FPGA_GetCore(pipeline->pos, 0);
+    return core->inputBuffer;
+}
+
+uint16_t* FPGAPipeline_GetOutputBuffer(FPGA_Pipeline *pipeline) {
+    FPGA_Core *core = FPGAPipline_GetCore(pipeline, pipeline->numCores - 1);
+    return core->outputBuffer;   
+}
+
+/*******************************************
+* FPGA Core methods                        *
+*******************************************/
+
+void FPGACore_GetProgram(FPGA_Core *core, uint16_t *program) {
+    for (uint32_t i = 0; i < core->imemSize; i++) {
+        program[i] = core->imem[i];
+    }
+}
+
+void FPGACore_SetProgram(FPGA_Core *core, uint16_t *program, uint32_t programSize) {
+    assert(programSize <= core->imemSize);
+
+    for (uint32_t i = 0; i < programSize; i++) {
+        core->imem[i] = program[i];
+    }
+    /* Set all remaining instruction memory to 0 */
+    if (programSize < core->imemSize) {
+        for (uint32_t i = programSize; i < core->imemSize; i++) {
+            core->imem[i] = 0;
+        }
+    }
+}
+
+/*******************************************
+* FPGA Setup and teardown                  *
+*******************************************/
+
 void FPGA_Init(FPGAConfig *config) {
     proc.baseAddress = config->baseAddress;
 
     proc.numPipelines = config->numPipelines;
-
     proc.pipelines = (FPGA_Pipeline *) malloc(sizeof(FPGA_Pipeline) * config->numPipelines);
-
     for (uint32_t i = 0; i < proc.numPipelines; i++) {
         FPGA_Pipeline_New(&proc.pipelines[i], i, config);
     }
@@ -40,61 +107,7 @@ void FPGA_Core_New(FPGA_Core *core, uint32_t corePos, uint32_t pipelinePos, FPGA
     core->imem = (core_base_address + core->bufferSize);
 }
 
-FPGA_Pipeline* FPGA_GetPipeline(uint32_t pipeline) {
-    if (pipeline < proc.numPipelines) {
-        return &proc.pipelines[pipeline];
-    }
-    return NULL;
-}
-
-FPGA_Core* FPGAPipline_GetCore(FPGA_Pipeline *pipeline, uint32_t core) {
-    if (pipeline != NULL && core < pipeline->numCores) {
-        return &pipeline->cores[core];
-    }
-    return NULL;
-}
-
-FPGA_Core* FPGA_GetCore(uint32_t pipeline, uint32_t core) {
-    FPGA_Pipeline *p = FPGA_GetPipeline(pipeline);
-    return FPGAPipline_GetCore(p, core);
-}
-
-uint16_t* FPGA_GetBaseAddress() {
-    return proc.baseAddress;
-}
-
-uint16_t* FPGAPipeline_GetInputBuffer(FPGA_Pipeline *pipeline) {
-    FPGA_Core *core = FPGA_GetCore(pipeline->pos, 0);
-    return core->inputBuffer;
-}
-
-uint16_t* FPGAPipeline_GetOutputBuffer(FPGA_Pipeline *pipeline) {
-    FPGA_Core *core = FPGA_GetCore(pipeline->pos, pipeline->numCores - 1);
-    return core->outputBuffer;   
-}
-
-void FPGACore_GetProgram(FPGA_Core *core, uint16_t *program) {
-    for (uint32_t i = 0; i < core->imemSize; i++) {
-        program[i] = *(core->imem + i);
-    }
-}
-
-void FPGACore_SetProgram(FPGA_Core *core, uint16_t *program, uint32_t programSize) {
-    assert(programSize <= core->imemSize);
-
-    for (uint32_t i = 0; i < programSize; i++) {
-        core->imem[i] = program[i];
-    }
-
-    /* Set all remaining instruction memory to 0 */
-    if (programSize < core->imemSize) {
-        for (uint32_t i = programSize; i < core->imemSize; i++) {
-            core->imem[i] = 0;
-        }
-    }
-}
-
-void FPGA_Destroy() {
+void FPGA_Destroy(void) {
     for (uint32_t i = 0; i < proc.numPipelines; i++) {
         FPGA_Pipeline *p = FPGA_GetPipeline(i);
         free(p->cores);
