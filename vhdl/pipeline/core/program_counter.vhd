464655dde3359e33b0f338b7faf9a9ecdde5664f
x10an14 <x10an14@hotmail.com>
2013-10-26 11:21:52 +0200

    Bugfix

--- vhdl/pipeline/core/program_counter.vhd
+++ vhdl/pipeline/core/program_counter.vhd
@@ -24,6 +24,8 @@ architecture behaviour of program_counter is
 
 	signal program_address : std_logic_vector(address_width -1 downto 0);
 
+begin
+
 	update_pc : process (clk, pc_wr_enb)
 	begin
 		if rising_edge(clk) then
