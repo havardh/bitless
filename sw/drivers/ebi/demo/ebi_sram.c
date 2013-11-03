072dfb683bda0a48e84d0a2cff3b1904c627bb8d
Sondre Lefsaker <sondrele@stud.ntnu.no>
2013-11-03 14:38:23 +0100

    added examples

--- sw/drivers/ebi/demo/ebi_sram.c
+++ sw/drivers/ebi/demo/ebi_sram.c
@@ -27,39 +27,39 @@ void Delay(uint32_t dlyTicks)
   while ((msTicks - curTicks) < dlyTicks) ;
 }
 
-#define EXT_SRAM_BASE_ADDRESS ((volatile uint16_t*) BC_PSRAM_BASE)
+// #define EXT_SRAM_BASE_ADDRESS ((volatile uint8_t*) BC_PSRAM_BASE)
 
 #define TEST_ADDRESS 0x100000
 
 #define TEST_ARRAY_SIZE 32
 
-uint16_t test[TEST_ARRAY_SIZE] = {
-    0x7BC1, 0x1EE2, 0x2E40, 0x9F96, 0xE93D, 0x7E11, 0x7393, 0x172A,
-    0xAE2D, 0x8A57, 0x1E03, 0xAC9C, 0x9EB7, 0x6FAC, 0x45AF, 0x8E51,
-    0x30C8, 0x1C46, 0xA35C, 0xE411, 0xE5FB, 0xC119, 0x1A0A, 0x52EF,
-    0xF69F, 0x2445, 0xDF4F, 0x9B17, 0xAD2B, 0x417B, 0xE66C, 0x3710 
+uint8_t test[TEST_ARRAY_SIZE] = {
+    0xC1, 0xE2, 0x40, 0x96, 0x3D, 0x11, 0x93, 0x2A,
+    0x2D, 0x57, 0x03, 0x9C, 0xB7, 0xAC, 0xAF, 0x51,
+    0xC8, 0x46, 0x5C, 0x11, 0xFB, 0x19, 0x0A, 0xEF,
+    0x9F, 0x45, 0x4F, 0x17, 0x2B, 0x7B, 0x6C, 0x10 
 };
 
-uint16_t answer[TEST_ARRAY_SIZE] = {
-    0x0000, 0x1111, 0x2222, 0x3333, 0x4444, 0x5555, 0x6666, 0x7777, 
-    0x8888, 0x9999, 0xAAAA, 0xBBBB, 0xCCCC, 0xDDDD, 0xEEEE, 0xFFFF,
-    0x0000, 0x1111, 0x2222, 0x3333, 0x4444, 0x5555, 0x6666, 0x7777, 
-    0x8888, 0x9999, 0xAAAA, 0xBBBB, 0xCCCC, 0xDDDD, 0xEEEE, 0xFFFF
+uint8_t answer[TEST_ARRAY_SIZE] = {
+    0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 
+    0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+    0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 
+    0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF
 };
 
-void write(uint16_t * data, int size) {
+void write(uint8_t * data, int size) {
     for (int i=0; i<size; i++) {
-        *(uint16_t*)(TEST_ADDRESS + i) = data[i];
+        *(uint8_t*)(TEST_ADDRESS + i) = data[i];
     }
 }
 
-void read(uint16_t *data, int size) {
+void read(uint8_t *data, int size) {
     for (int i=0; i<size; i++) {
-        data[i] = *(uint16_t*)(TEST_ADDRESS + i);
+        data[i] = *(uint8_t*)(TEST_ADDRESS + i);
     }
 }
 
-bool test_result(uint16_t *a, uint16_t *b, int size) {
+bool test_result(uint8_t *a, uint8_t *b, int size) {
     bool result = true;
     for (int i=0; i<size; i++) {
         if (a[i] != b[i]) {
@@ -71,8 +71,8 @@ bool test_result(uint16_t *a, uint16_t *b, int size) {
 
 int main( void ) {
 
-  CHIP_Init();
-  if (SysTick_Config(CMU_ClockFreqGet(cmuClock_CORE) / 1000)) while (1) ;
+    CHIP_Init();
+    if (SysTick_Config(CMU_ClockFreqGet(cmuClock_CORE) / 1000)) while (1) ;
 
     // BSP_Init( BSP_INIT_DEFAULT );
 
@@ -86,6 +86,8 @@ int main( void ) {
 
     bool result = test_result(test, answer, TEST_ARRAY_SIZE);
 
+    BSP_Init(BSP_INIT_DK_SPI);
+
     while(1) {
         BSP_LedsSet(0x0);
         Delay(1000);
@@ -93,7 +95,7 @@ int main( void ) {
         if (result) {
             BSP_LedsSet(0xFF);
         } else {
-            BSP_LedsSet(0xFF00);
+            BSP_LedsSet(0x0001);
         }
 
         Delay(1000);
