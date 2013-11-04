d593e64da7253449f3cfaeee91f6c9fd87263723
Sondre Lefsaker <sondrele@stud.ntnu.no>
2013-11-04 20:49:21 +0100

    removed warnings of unused variables

--- sw/controllers/leds/src/leds.c
+++ sw/controllers/leds/src/leds.c
@@ -3,9 +3,8 @@
 static LED_t LEDS[NUM_LEDS] = LEDS_ARRAY_INIT;
 
 uint32_t LEDController_GetLeds(void) {
-    uint32_t ledsVal, mask;
-
-    for (int i=0, ledsVal=0, mask=0x1; i < NUM_LEDS; i++, mask <<= 1) {
+    uint32_t ledsVal = 0;
+    for (uint32_t i = 0, mask = 0x1; i < NUM_LEDS; i++, mask <<= 1) {
         if (GPIO_PinOutGet(LEDS[i].port, LEDS[i].pin))
             ledsVal |= mask;
     }
@@ -26,9 +25,7 @@ void LEDController_ClearLed(uint32_t led) {
 }
 
 void LEDController_SetLeds(uint32_t leds) {
-    uint32_t mask;
-
-    for (int i = 0, mask = 0x1; i < NUM_LEDS; i++, mask <<= 1 ) {
+    for (uint32_t i = 0, mask = 0x1; i < NUM_LEDS; i++, mask <<= 1 ) {
         if (leds & mask) {
             GPIO_PinOutSet(LEDS[i].port, LEDS[i].pin);
         } else {
