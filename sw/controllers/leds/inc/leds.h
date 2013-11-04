bc809fcb17b4b068377ec62454ce85a90e9bfd42
Sondre Lefsaker <sondrele@stud.ntnu.no>
2013-11-04 20:07:58 +0100

    Bugfixes, now working on STK

--- sw/controllers/leds/inc/leds.h
+++ sw/controllers/leds/inc/leds.h
@@ -8,31 +8,17 @@
 #include "em_cmu.h"
 #include "em_emu.h"
 #include "em_gpio.h"
-
-#define NUM_LEDS        5
-#define LED0            7
-#define LED1            8
-#define LED2            9
-#define LED3           10
-#define LED4           11
-
-#define LEDS_ARRAY_INIT {\
-    {gpioPortA, (LED0)}, \
-    {gpioPortA, (LED1)}, \
-    {gpioPortA, (LED2)}, \
-    {gpioPortA, (LED3)}, \
-    {gpioPortA, (LED4)}, \
-}
+#include "leds_config.h"
 
 typedef struct {
   GPIO_Port_TypeDef   port;
   unsigned int        pin;
 } LED_t;
 
-void SysTick_Handler(void);
-void LEDController_ClearLed(uint32_t led);
+void LEDController_Init(void);
+uint32_t LEDController_GetLeds(void);
 void LEDController_SetLed(uint32_t led);
 void LEDController_SetLeds(uint32_t leds);
-void LEDController_Init(void);
+void LEDController_ClearLed(uint32_t led);
 
 #endif // _LEDSCONTROLLER_H_
--- sw/controllers/leds/src/leds.c
+++ sw/controllers/leds/src/leds.c
@@ -2,14 +2,25 @@
 
 static LED_t LEDS[NUM_LEDS] = LEDS_ARRAY_INIT;
 
+uint32_t LEDController_GetLeds(void) {
+    uint32_t ledsVal, mask;
+
+    for (int i=0, ledsVal=0, mask=0x1; i < NUM_LEDS; i++, mask <<= 1) {
+        if (GPIO_PinOutGet(LEDS[i].port, LEDS[i].pin))
+            ledsVal |= mask;
+    }
+
+    return ledsVal;
+}
+
 void LEDController_SetLed(uint32_t led) {
-    if (led > 0 && led < NUM_LEDS) {
+    if (led < NUM_LEDS) {
         GPIO_PinOutSet(LEDS[led].port, LEDS[led].pin);
     }
 }
 
 void LEDController_ClearLed(uint32_t led) {
-    if (led > 0 && led < NUM_LEDS) {
+    if (led < NUM_LEDS) {
         GPIO_PinOutClear(LEDS[led].port, LEDS[led].pin);
     }   
 }
