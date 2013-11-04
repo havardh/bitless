a965ead1df14a51f0856daea9a76ff12ccc035d4
magnhalv <MagnusHalvorsen1991@gmail.com>
2013-11-04 12:13:18 +0100

    Fixed forward unit

--- vhdl/pipeline/core/core.vhd
+++ vhdl/pipeline/core/core.vhd
@@ -38,7 +38,7 @@ entity core is
 		output_we			: out std_logic;
 		
 		output_read_address	: out std_logic_vector(address_width - 1 downto 0);
-		output_read_data	: in  std_logic_vector(31 downto 0);
+		output_read_data	: in  std_logic_vector(31 downto 0)
  	);
 end entity;
 
@@ -93,28 +93,36 @@ architecture behaviour of core is
 			write_reg_enb	: in register_write_enable;
 
 			reg_1_data		: out std_logic_vector(15 downto 0);
-			reg_2_data		: out std_logic_vector(15 downto 0);
+			reg_2_data		: out std_logic_vector(15 downto 0)
 		);
 	end component register_file;
-	--Pipeline registers for stage 2
-	signal reg_out_data_1	: std_logic_vector(15 downto 0);
-	signal reg_out_data_2	: std_logic_vector(15 downto 0);
+	--Pipeline registers for stage ID
+	signal id_reg_data_1	: std_logic_vector(15 downto 0);
+	signal id_reg_data_2	: std_logic_vector(15 downto 0);
 	
-	signal spec_addr		: std_logic_vector( 4 downto 0);
-	signal reg_addr_1		: std_logic_vector( 4 downto 0);
-	signal reg_addr_2		: std_logic_vector( 4 downto 0);
-	signal reg_write_addr	: std_logic_vector( 4 downto 0);
-	signal alu_op			: alu_operation;
-	signal imm_slct			: std_logic;
-	signal reg_we			: std_logic;
-	signal reg_wb			: std_logic;
-	signal reg_w_src		: std_logic_vector(1 downto 0);
+	signal id_spec_addr			: std_logic_vector( 4 downto 0);
+	signal id_reg_addr_1			: std_logic_vector( 4 downto 0);
+	signal id_reg_addr_2			: std_logic_vector( 4 downto 0);
+	signal id_reg_write_addr	: std_logic_vector( 4 downto 0);
+	signal id_alu_op				: alu_operation;
+	signal id_imm_slct			: std_logic;
+	signal id_reg_we				: std_logic;
+	signal id_reg_wb				: std_logic;
+	signal id_reg_w_src			: std_logic_vector(1 downto 0);
 	
 		
---Stage 3 - Memory access
+--Stage 3 - Memory access / MEM
 	component forwarding_unit is
-	end component;
-	--Pipeline registers for stage 3
+		Port (
+			wb_reg					: in STD_LOGIC_VECTOR(4 downto 0); 
+			reg_addr_1			  	: in STD_LOGIC_VECTOR(4 downto 0); 
+			reg_addr_2			 	: in STD_LOGIC_VECTOR(4 downto 0); 
+			reg_write   			: in STD_LOGIC;
+			forward_1         	: out STD_LOGIC;
+			forward_2         	: out STD_LOGIC
+		);
+	end component forwarding_unit;
+	--Pipeline registers for stage EX
 		--TODO
 		
 --Stage 4 - ALU
@@ -134,8 +142,20 @@ architecture behaviour of core is
 			flags  					: out	alu_flags
 		);
 	end component;
-	--Pipeline registers for stage 4
-		--TODO
+	
+	component forwarding_unit is
+		Port (
+			wb_reg					: in STD_LOGIC_VECTOR(4 downto 0); -- Hazard type 1
+			reg_addr_1			  	: in STD_LOGIC_VECTOR(4 downto 0); -- Hazard type a
+			reg_addr_2			 	: in STD_LOGIC_VECTOR(4 downto 0); -- Hazard type b
+			reg_write   			: in STD_LOGIC;
+			forward_1         	: out STD_LOGIC;
+			forward_2         	: out STD_LOGIC
+		);
+	end component forwarding_unit;
+	--Pipeline registers for stage WB
+	
+	signal 
 	
 --Other
 
@@ -170,15 +190,13 @@ begin
 		reset				=> reset,
 		
 		opt_code			=> instruction(15 downto 10),
-		spec_reg_addr		=> spec_addr,
-		imm_select	 		=> imm_slct,
-		reg_write_e			=> reg_we,
-		reg_b_wr			=> reg_wb,
-		reg_write_source 	=> reg_src,
-		output_write_enable => output_we,
-		read_from_const_mem => '-',
-		branch_enable		=> branch_enable,
-		pc_write_enable 	=> pc_we
+		spec_reg_addr		=> id_spec_addr,
+		imm_select	 		=> id_imm_slct,
+		reg_write_e			=> id_reg_we,
+		reg_b_wr			=> id_reg_wb,
+		reg_write_source 	=> id_reg_src,
+		output_write_enable => id_output_we,
+		branch_enable		=> branch_enable
 	);
 	
 	regfile : register_file
@@ -187,14 +205,14 @@ begin
 
 		reg_1_address	=> instruction_data(9 downto 5),
 		reg_2_address	=> instruction_data(4 downto 0),
-		write_address	=> reg_write_addr,
+		write_address	=> wb_reg_write_addr,
 
-		data_in			=> stage_5_alu_mem_result,
+		data_in			=> wb_data,
 
-		write_reg_enb	=> reg_we,
+		write_reg_enb	=> wb_reg_we,
 
-		reg_1_data		=> reg_out_data_1,
-		reg_2_data		=> reg_out_data_2
+		reg_1_data		=> id_reg_data_1,
+		reg_2_data		=> id_reg_data_2
 	);
 	--TODO: Pipeline registerz for signals
 	
--- vhdl/pipeline/core/forwarding_unit.vhd
+++ vhdl/pipeline/core/forwarding_unit.vhd
@@ -4,14 +4,12 @@ use WORK.MIPS_CONSTANT_PKG.all;
 
 entity forwarding_unit is
     Port (
-        ex_mem_RegisterRd : in STD_LOGIC_VECTOR(4 downto 0); -- Hazard type 1
-        mem_wb_RegisterRd : in STD_LOGIC_VECTOR(4 downto 0); -- Hazard type 2
-        id_ex_RegisterRs  : in STD_LOGIC_VECTOR(4 downto 0); -- Hazard type a
-        id_ex_RegisterRt  : in STD_LOGIC_VECTOR(4 downto 0); -- Hazard type b
-        ex_mem_RegWrite   : in STD_LOGIC;
-        mem_wb_RegWrite   : in STD_LOGIC;
-        forward_b         : out STD_LOGIC_VECTOR(1 downto 0);
-        forward_a         : out STD_LOGIC_VECTOR(1 downto 0)
+			wb_reg					: in STD_LOGIC_VECTOR(4 downto 0); -- Hazard type 1
+			reg_addr_1			  	: in STD_LOGIC_VECTOR(4 downto 0); -- Hazard type a
+			reg_addr_2			 	: in STD_LOGIC_VECTOR(4 downto 0); -- Hazard type b
+			reg_write   			: in STD_LOGIC;
+			forward_1         	: out STD_LOGIC;
+			forward_2         	: out STD_LOGIC
     );
 end forwarding_unit;
 
@@ -19,33 +17,20 @@ architecture Behavioral of forwarding_unit is
 
 begin
 
-forward_signals : process(ex_mem_RegWrite, mem_wb_RegWrite, ex_mem_RegisterRd, mem_wb_RegisterRd, id_ex_RegisterRs, id_ex_RegisterRt)
+forward_signals : process(reg_addr_1, reg_addr_2, reg_write, wb_reg)
 
 begin
     -- EX hazard
-    if (ex_mem_RegWrite = '1') and not(ex_mem_RegisterRd = "00000") then
-        if (ex_mem_RegisterRd = id_ex_RegisterRs) then -- Hazard type 1a
-            forward_a <= "10";
-            forward_b <= "00";
-        elsif (ex_mem_RegisterRd = id_ex_RegisterRt) then -- Hazard type 1b
-            forward_a <= "00";
-            forward_b <= "10";
+    if (reg_write = '1') then
+        if (wb_reg = reg_addr_1) then 
+            forward_1 <= "1";
+            forward_2 <= "0";
+        elsif (web_reg = reg_addr_2) then 
+            forward_1 <= "0";
+            forward_2 <= "1";
         else
-            forward_a <= "00";
-            forward_b <= "00";
-        end if;
-    -- MEM Hazard
-    elsif (mem_wb_RegWrite = '1') and not(mem_wb_RegisterRd = "00000")
-    and not((ex_mem_RegWrite = '1') and not(ex_mem_RegisterRd = "00000")) then
-        if (mem_wb_RegisterRd = id_ex_RegisterRs) and not(ex_mem_RegisterRd = id_ex_RegisterRs) then -- Hazard type 2a
-            forward_a <= "01";
-            forward_b <= "00";
-        elsif (mem_wb_RegisterRd = id_ex_RegisterRt) and not(ex_mem_RegisterRd = id_ex_RegisterRt) then -- Hazard type 2b
-            forward_a <= "00";
-            forward_b <= "01";
-        else
-            forward_a <= "00";
-            forward_b <= "00";
+            forward_1 <= "0";
+            forward_2 <= "0";
         end if;
     else
         forward_a <= "00";
