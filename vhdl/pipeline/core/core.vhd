5ce7956c54a2aeadd15dfbe5c09a591dc208520a
Olav Emil Eiksund <olavemil@stud.ntnu.no>
2013-11-04 16:54:21 +0100

    Fixed core. Depends on FWU and CU being fixed. :)

--- vhdl/pipeline/core/core.vhd
+++ vhdl/pipeline/core/core.vhd
@@ -37,7 +37,7 @@ entity core is
 		output_write_data	: out std_logic_vector(31 downto 0);
 		output_we			: out std_logic;
 		
-		output_read_address	: out std_logic_vector(address_width - 1 downto 0);
+		output_read_addr	: out std_logic_vector(address_width - 1 downto 0);
 		output_read_data	: in  std_logic_vector(31 downto 0)
  	);
 end entity;
@@ -62,7 +62,7 @@ architecture behaviour of core is
 	signal pc_we			: std_logic;
 	
 	signal branch_enable	: std_logic;
-	signal branch_val		: std_logic_vector(15 downto 0);
+	signal branch_target	: std_logic_vector(15 downto 0);
 	
 --Stage 2 - Control unit and register file
 	component control_unit is
@@ -74,7 +74,7 @@ architecture behaviour of core is
 			spec_reg_addr		: out	std_logic_vector (4 downto 0);
 			alu_op				: out	alu_operation;
 			imm_select	 		: out	std_logic;
-			reg_write_e			: out	std_logic;
+			reg_write_e			: out	register_write_enable;
 			reg_b_wr			: out	std_logic;
 			reg_write_source 	: out	std_logic_vector (1 downto 0);
 			output_write_enable : out	std_logic;
@@ -104,29 +104,47 @@ architecture behaviour of core is
 	signal id_reg_data_1	: std_logic_vector(15 downto 0);
 	signal id_reg_data_2	: std_logic_vector(15 downto 0);
 	
-	signal id_spec_addr			: std_logic_vector( 4 downto 0);
-	signal id_reg_addr_1		: std_logic_vector( 4 downto 0);
-	signal id_reg_addr_2		: std_logic_vector( 4 downto 0);
-	signal id_reg_write_addr	: std_logic_vector( 4 downto 0);
-	signal id_alu_op			: alu_operation;
-	signal id_imm_slct			: std_logic;
-	signal id_reg_we			: std_logic;
-	signal id_reg_wb			: std_logic;
-	signal id_reg_w_src			: std_logic_vector(1 downto 0);
+	signal id_spec_addr					: std_logic_vector( 4 downto 0);
+	signal id_reg_addr_1				: std_logic_vector( 4 downto 0);
+	signal id_reg_addr_2				: std_logic_vector( 4 downto 0);
+	signal id_reg_write_addr			: std_logic_vector( 4 downto 0);
+	signal id_alu_op					: alu_operation;
+	signal id_imm_slct					: std_logic;
+	signal id_reg_we					: register_write_enable;
+	signal id_reg_wb					: std_logic;
+	signal id_reg_src					: std_logic_vector(1 downto 0);
+	signal id_output_we					: std_logic;
+	signal id_alu_result_mux_control	: data_select;
+	signal id_input_data_mux_control	: mem_select;
 	
-		
+	
 --Stage 3 - Memory access / MEM
 	component forwarding_unit is
 		Port (
 			wb_reg				: in STD_LOGIC_VECTOR(4 downto 0); 
 			reg_addr_1			: in STD_LOGIC_VECTOR(4 downto 0); 
 			reg_addr_2			: in STD_LOGIC_VECTOR(4 downto 0); 
-			reg_write			: in STD_LOGIC;
+			reg_write			: in register_write_enable;
 			forward_1			: out STD_LOGIC;
 			forward_2			: out STD_LOGIC
 		);
 	end component forwarding_unit;
 	--Pipeline registers for stage EX
+	signal mem_register_1_data			: std_logic_vector(15 downto 0);
+	signal mem_register_2_data			: std_logic_vector(15 downto 0);
+	signal mem_alu_result_mux_control	: data_select;
+	signal mem_imm_data					: std_logic_vector(15 downto 0);
+	signal mem_reg_1_addr				: std_logic_vector(4 downto 0);
+	signal mem_reg_2_addr				: std_logic_vector(4 downto 0);
+	signal mem_reg_we					: register_write_enable;
+	signal mem_addr						: std_logic_vector(15 downto 0);
+	signal mem_data						: std_logic_vector(31 downto 0);
+	signal mem_addr_mux_control			: std_logic;
+	signal mem_output_data_mux_control	: std_logic;
+	signal mem_input_data_mux_control	: mem_select;
+	
+	
+	
 		--TODO
 		
 --Stage 4 - ALU
@@ -146,9 +164,34 @@ architecture behaviour of core is
 			flags  					: out	alu_flags
 		);
 	end component;
+	
+	
+	signal alu_input_1_mux_out			: std_logic_vector(31 downto 0);
+	signal alu_input_1_mux_control		: std_logic;
+	signal alu_input_2_mux_out			: std_logic_vector(31 downto 0);
+	signal alu_input_2_mux_control		: std_logic;
+	signal alu_result					: std_logic_vector(31 downto 0);
+	signal alu_result_mux_out			: std_logic_vector(31 downto 0);
+	
+	--Pipeline registers for stage EX
+	signal ex_mem_data					: std_logic_vector(31 downto 0);
+	signal ex_reg_1_addr				: std_logic_vector(15 downto 0); 
+	signal ex_reg_2_addr				: std_logic_vector(15 downto 0);
+	signal ex_register_1_data			: std_logic_vector(31 downto 0);
+	signal ex_register_2_data			: std_logic_vector(31 downto 0);
+	signal ex_alu_result_mux_control	: data_select;
+	signal ex_imm_data					: std_logic_vector(15 downto 0);
+	signal ex_reg_we					: register_write_enable;
+	signal ex_alu_op					: alu_operation;
+	signal ex_constant_we				: std_logic;
+	signal ex_flags						: alu_flags;
+	
 
 	--Pipeline registers for stage WB
-	
+	signal wb_data						: std_logic_vector(31 downto 0);
+	signal wb_reg						: std_logic_vector(4 downto 0);
+	signal wb_reg_we					: register_write_enable;
+	signal wb_flags						: alu_flags;
 	
 --Other
 
@@ -158,7 +201,7 @@ begin
 	pc_incrementer : adder
 	port map(
 		a		=> pc_reg,
-		b		=> '1',
+		b		=> "0001",
 		result	=> pc_inc
 	);
 	
@@ -168,7 +211,7 @@ begin
 			if branch_enable = '1' then
 				pc_reg <= branch_target;
 			else
-				pr_reg <= pc_inc;
+				pc_reg <= pc_inc;
 			end if;
 		end if;
 	end process;
@@ -183,7 +226,7 @@ begin
 		clk					=> clk,
 		reset				=> reset,
 		
-		opt_code			=> instruction(15 downto 10),
+		opt_code			=> instruction_data(15 downto 10),
 		spec_reg_addr		=> id_spec_addr,
 		imm_select	 		=> id_imm_slct,
 		reg_write_e			=> id_reg_we,
@@ -199,14 +242,14 @@ begin
 
 		reg_1_address	=> instruction_data(9 downto 5),
 		reg_2_address	=> instruction_data(4 downto 0),
-		write_address	=> wb_reg_write_addr,
+		write_address	=> wb_reg,
 
 		data_in			=> wb_data,
 
 		write_reg_enb	=> wb_reg_we,
 
-		reg_1_data		=> mem_reg_data_1,
-		reg_2_data		=> mem_reg_data_2
+		reg_1_data		=> mem_register_1_data,
+		reg_2_data		=> mem_register_2_data
 	);
 	
 	--Pipeline registerz for signals
@@ -214,20 +257,22 @@ begin
 	begin
 		if rising_edge(clk) then
 			mem_alu_result_mux_control	<= id_alu_result_mux_control;
+			mem_input_data_mux_control	<= id_input_data_mux_control;
 			
 			mem_imm_data	<= instruction_data(13 downto 0);
 			
 			mem_reg_1_addr	<= instruction_data(9 downto 5);
 			mem_reg_2_addr	<= instruction_data(4 downto 0);
 			mem_reg_we		<= id_reg_we;
+			
 		end if;
 	end process;
 	
 --Pipeline stage 3 MEM
 	mem_addr_mux : process(mem_addr_mux_control)
 	begin
-		if mem_addr_muc_control = '1' then
-			mem_addr <= mem_reg_data_1;
+		if mem_addr_mux_control = '1' then
+			mem_addr <= mem_register_1_data;
 		else
 			mem_addr <= wb_data;
 		end if;
@@ -241,25 +286,25 @@ begin
 	mem_output_data_mux : process(mem_output_data_mux_control)
 	begin
 		if mem_output_data_mux_control = '1' then
-			output_write_data <= mem_reg_data_2;
+			output_write_data <= mem_register_2_data;
 		else
-			output_write_data <= mem_reg_addr_1 & mem_reg_addr_2;
+			output_write_data <= mem_reg_1_addr & mem_reg_1_addr;
 		end if;
 	end process;
 	
 	mem_input_data_mux : process(mem_input_data_mux_control)
 	begin
 		case mem_input_data_mux_control is
-			when const =>
+			when mem_const =>
 				ex_mem_data <= constant_data;
-			when input =>
+			when mem_input =>
 				ex_mem_data <= input_read_data;
-			when output =>
+			when mem_output =>
 				ex_mem_data <= output_read_data;
 		end case;
 	end process;
 	--Forwarding unit
-	ex_forwarding_unit : forwarding_unit
+	mem_forwarding_unit : forwarding_unit
 	port map(
 		wb_reg		=> wb_reg,
 		reg_addr_1	=> ex_reg_1_addr,
@@ -300,11 +345,11 @@ begin
 		if alu_input_2_mux_control = '0' then
 			alu_input_2_mux_out <= ex_register_2_data;
 		else
-			alu_input_2_mux_out <= wb_alu_mem_result;
+			alu_input_2_mux_out <= wb_data;
 		end if;
 	end process;
 	
-	alu : alu
+	alu_unit : alu
 	port map(
 		-- CLK
 		dsp_clk					=> '-',
@@ -324,11 +369,11 @@ begin
 	alu_result_mux : process(ex_alu_result_mux_control)
 	begin
 		case ex_alu_result_mux_control is
-		when alu =>
+		when mux_alu =>
 			alu_result_mux_out <= alu_result;
-		when mem =>
+		when mux_mem =>
 			alu_result_mux_out <= ex_mem_data;
-		when imm => 
+		when mux_imm => 
 			alu_result_mux_out <= ex_imm_data;
 		when others =>
 		end case;
@@ -349,7 +394,7 @@ begin
 		if rising_edge(clk) then
 			wb_reg		<= ex_reg_1_addr;
 			wb_reg_we	<= ex_reg_we;
-			wb_flags	<= alu_flags;
+			wb_flags	<= ex_flags;
 			wb_data		<= alu_result_mux_out;
 		end if;
 	end process;
