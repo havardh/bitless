2855110d15ac2050b112fff954863f0a183e0436
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-11-07 21:57:32 +0100

    Update sample conversion with tests

--- sw/libs/util/inc/sample_conversion.h
+++ sw/libs/util/inc/sample_conversion.h
@@ -4,9 +4,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#define MID_POINT 0x8000
+#define BIT_SHIFT_LENGTH 8
+
 void signedToUnsigned(int16_t *samples, size_t n);
-void unsignedToSigned(int16_t *samples, size_t n);
+void unsignedToSigned(uint16_t *samples, size_t n);
 
 void downScale(uint16_t *samples, size_t n);
+void upScale(uint16_t *samples, size_t n);
 
 #endif /* _SAMPLE_CONVERSION_H_ */
--- sw/libs/util/src/sample_conversion.c
+++ sw/libs/util/src/sample_conversion.c
@@ -2,24 +2,32 @@
 
 void signedToUnsigned(int16_t *samples, size_t n) {
 
-	for (int i=0; i<n; i++) {
-		((uint16_t*)samples)[i] = ((int32_t)samples[i]) + 0x8000;
+	for (size_t i=0; i<n; i++) {
+		((uint16_t*)samples)[i] = (uint16_t) ((int32_t)samples[i] + MID_POINT);
 	}
 
 }
 
-void unsignedToSigned(int16_t *samples, size_t n) {
+void unsignedToSigned(uint16_t *samples, size_t n) {
 
-	for (int i=0; i<n; i++) {
-		((int16_t*)samples)[i] = ((int32_t)samples[i]) - 0x8000;
+	for (size_t i=0; i<n; i++) {
+		((int16_t*)samples)[i] = (int16_t)((int32_t)samples[i] - MID_POINT);
 	}
 
 }
 
 void downScale(uint16_t *samples, size_t n) {
 
-	for (int i=0; i<n; i++) {
-		samples[i] >>= 4;
+	for (size_t i=0; i<n; i++) {
+		samples[i] >>= BIT_SHIFT_LENGTH;
+	}
+
+}
+
+void upScale(uint16_t *samples, size_t n) {
+
+	for (size_t i=0; i<n; i++) {
+		samples[i] <<= BIT_SHIFT_LENGTH;
 	}
 
 }
