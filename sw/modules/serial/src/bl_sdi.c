fff364e20407f97dd63401d81c2ac4d0037908a6
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-11-10 17:40:43 +0100

    Updated test to statically allocated

--- sw/modules/serial/src/bl_sdi.c
+++ sw/modules/serial/src/bl_sdi.c
@@ -14,16 +14,22 @@ typedef struct {
 
 static Pipeline *pipelines;
 
+static uint8_t nums[32] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+
 void SDI_Init(void) 
 {
-	pipelines = (Pipeline*)malloc(sizeof(Pipeline)*2);
-	for (int i=0; i<2; i++) {
-		pipelines[i].cores = (Core*)malloc(sizeof(Core)*2);
+	pipelines = (Pipeline*)malloc(sizeof(Pipeline)*1);
+	for (int i=0; i<1; i++) {
+		pipelines[i].cores = (Core*)malloc(sizeof(Core)*1);
 
-		for (int j=0; j<2; j++) {
-			pipelines[i].cores[j].in = malloc(sizeof(uint16_t)*MEM_SIZE);
-			pipelines[i].cores[j].out = malloc(sizeof(uint16_t)*MEM_SIZE);
-			pipelines[i].cores[j].inst = malloc(sizeof(uint16_t)*MEM_SIZE);
+		for (int j=0; j<1; j++) {
+			pipelines[i].cores[j].in = (uint16_t*)malloc(sizeof(uint16_t)*16);
+			//pipelines[i].cores[j].out = nums;
+			//pipelines[i].cores[j].inst = nums;
+
+			for(int k=0; k<16; k++) {
+				pipelines[i].cores[j].in[k] = nums[k];
+			}
 		}
 	}
 }
@@ -52,7 +58,7 @@ void write_instruction(uint8_t command[])
 
 	uint16_t *inst = pipelines[pipeline].cores[core].inst;
 
-	UART_GetData(inst, 16*2);
+	UART_GetData((uint8_t*)inst, 16*2);
 }
 
 void write_data(uint8_t command[])
@@ -62,7 +68,7 @@ void write_data(uint8_t command[])
 
 	uint16_t *mem = pipelines[pipeline].cores[memory].in;
 	
-	UART_GetData(mem, 16*2);
+	UART_GetData((uint8_t*)nums, 32);
 }
 
 void read_data(uint8_t command[])
@@ -72,7 +78,7 @@ void read_data(uint8_t command[])
 
 	uint16_t *mem = pipelines[pipeline].cores[memory].in;
 
-	UART_PutData(mem, 16*2);
+	UART_PutData((uint8_t*)nums, 32);
 }
 
 
