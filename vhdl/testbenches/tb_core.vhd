7e00c21e2005fc1482e00b695659ca152d418ba2
x10an14 <x10an14@hotmail.com>
2013-11-13 19:22:40 +0100

    Written all 8 tests, haven't been able to attempt testing yet due to some wrong word lengths. Pushing to fix this quicker on another pc.

--- vhdl/testbenches/tb_core.vhd
+++ vhdl/testbenches/tb_core.vhd
@@ -103,24 +103,24 @@ ARCHITECTURE behavior OF tb_core IS
    --and all loads will have to check the register files and have the value ready on the inputlines on core when the instruction is run.
    
      --Store instructions
-   constant store_outpt_1 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"10101"&"00010"; --Store value in register 13 into the output address register 2's value poins to
-   constant store_outpt_2 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"11111"&"00011"; --Store value in register 31 into the output address register 3's value poins to
-   constant store_outpt_3 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"00000"&"00100"; --Store value in register 0 into the output address register 4's value poins to
-   constant store_outpt_4 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"01010"&"00101"; --Store value in register 8 into the output address register 5's value poins to
-   constant store_outpt_5 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"11011"&"00110"; --Store value in register 27 into the output address register 6's value poins to
-   constant store_outpt_6 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"00100"&"00111"; --Store value in register 4 into the output address register 7's value poins to
-   constant store_outpt_7 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"00111"&"01000"; --Store value in register 7 into the output address register 8's value poins to
-   constant store_outpt_8 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"11000"&"01001"; --Store value in register 24 into the output address register 9's value poins to
+   constant store_outpt_1 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"10101"&"00010"; --Store value in register 13 into the output address register 2's value points to
+   constant store_outpt_2 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"11111"&"00011"; --Store value in register 31 into the output address register 3's value points to
+   constant store_outpt_3 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"00000"&"00100"; --Store value in register 0 into the output address register 4's value points to
+   constant store_outpt_4 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"01010"&"00101"; --Store value in register 8 into the output address register 5's value points to
+   constant store_outpt_5 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"11011"&"00110"; --Store value in register 27 into the output address register 6's value points to
+   constant store_outpt_6 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"00100"&"00111"; --Store value in register 4 into the output address register 7's value points to
+   constant store_outpt_7 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"00111"&"01000"; --Store value in register 7 into the output address register 8's value points to
+   constant store_outpt_8 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_store &"11000"&"01001"; --Store value in register 24 into the output address register 9's value points to
    
      --Load instructions
-   constant load_inpt_1 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"00010"&"00000"; --Load the value pointed to by value in address 0 to register 2
-   constant load_inpt_2 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"00011"&"00000"; --Load the value pointed to by value in address 0 to register 3
-   constant load_inpt_3 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"00100"&"00000"; --Load the value pointed to by value in address 0 to register 4
-   constant load_inpt_4 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"00101"&"00000"; --Load the value pointed to by value in address 0 to register 5
-   constant load_inpt_5 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"00110"&"00000"; --Load the value pointed to by value in address 0 to register 6
-   constant load_inpt_6 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"00111"&"00000"; --Load the value pointed to by value in address 0 to register 7
-   constant load_inpt_7 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"01000"&"00000"; --Load the value pointed to by value in address 0 to register 8
-   constant load_inpt_8 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"01001"&"00000"; --Load the value pointed to by value in address 0 to register 9
+   constant load_inpt_1 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"00010"&"00000"; --Load the value pointed to by the address in register 0 to register 2. Value loads to input buffer
+   constant load_inpt_2 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"00011"&"00000"; --Load the value pointed to by the address in register 0 to register 3. Value loads to input buffer
+   constant load_inpt_3 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load1 &"00100"&"00000"; --Load the value pointed to by the address in register 0 to register 4. Value loads to output buffer
+   constant load_inpt_4 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load2 &"00101"&"00000"; --Load the value pointed to by the address in register 0 to register 5. Value loads to output buffer
+   constant load_inpt_5 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load2 &"00110"&"00000"; --Load the value pointed to by the address in register 0 to register 6. Value loads to output buffer
+   constant load_inpt_6 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load3 &"00111"&"00000"; --Load the value pointed to by the address in register 0 to register 7. Value loads to constant buffer
+   constant load_inpt_7 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load3 &"01000"&"00000"; --Load the value pointed to by the address in register 0 to register 8. Value loads to constant buffer
+   constant load_inpt_8 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load3 &"01001"&"00000"; --Load the value pointed to by the address in register 0 to register 9. Value loads to constant buffer
  
 BEGIN 
  
@@ -185,13 +185,82 @@ BEGIN
 --		--When commands are run, you need one clockcycle between each command, like shown below
 --		--First instruction
 --		instruction_data <= instruction_1;
---		wait for clk_period;
+--		wait for clk_period*2;
+--
+--		--Check if instruction worked
+--		wait for clk_period*2;
 --		
 --		--Next instruction
 --		instruction_data <= instruction_2;
 --		wait for clk_period;
-		
+--
+--		--Check if instruction worked
+--		wait for clk_period*2;
+--		
 		--And so on. Between each instruction check the corresponding registers or input-/output-buffers to verify instruction.
+		
+		--Load value from input buffer to register 2
+		input_read_data <= X"1010";
+		instruction_data <= load_inpt_1;
+		wait for clk_period*2;
+		
+		--Check if register 2 has the above hex value
+		wait for clk_period*2;
+		
+		--Load value from input buffer to register 3
+		input_read_data <= X"0101";
+		instruction_data <= load_inpt_2;
+		wait for clk_period*2;
+		
+		--Check if register 3 has the above hex value
+		wait for clk_period*2;
+		
+		--Load value from input buffer to register 4
+		input_read_data <= X"0011";
+		instruction_data <= load_inpt_3;
+		wait for clk_period*2;
+		
+		--Check if register 4 has the above hex value
+		wait for clk_period*2;
+		
+		--Load value from input buffer to register 5
+		constant_data <= X"1010";
+		instruction_data <= load_inpt_4;
+		wait for clk_period*2;
+		
+		--Check if register 5 has the above hex value
+		wait for clk_period*2;
+		
+		--Load value from input buffer to register 6
+		constant_data <= X"0101";
+		instruction_data <= load_inpt_5;
+		wait for clk_period*2;
+		
+		--Check if register 6 has the above hex value
+		wait for clk_period*2;
+		
+		--Load value from input buffer to register 7
+		output_read_data <= X"1010";
+		instruction_data <= load_inpt_6;
+		wait for clk_period*2;
+		
+		--Check if register 7 has the above hex value
+		wait for clk_period*2;
+		
+		--Load value from input buffer to register 8
+		output_read_data <= X"0101";
+		instruction_data <= load_inpt_7;
+		wait for clk_period*2;
+		
+		--Check if register 8 has the above hex value
+		wait for clk_period*2;
+		
+		--Load value from input buffer to register 9
+		output_read_data <= X"0011";
+		instruction_data <= load_inpt_8;
+		wait for clk_period*2;
+		
+		--Check if register 9 has the above hex value
 		
 
       wait;
