60b9e1028b2babfefd8d78af326152be08080aa5
magnhalv <MagnusHalvorsen1991@gmail.com>
2013-11-14 16:54:03 +0100

    fixed reset

--- vhdl/pipeline/hazard_dectection_unit.vhd
+++ vhdl/pipeline/hazard_dectection_unit.vhd
@@ -30,12 +30,36 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity hazard_dectection_unit is
+    Port (
+        clk         : in std_logic;
+        mem_branch  : in std_logic;
+        ex_branch   : in std_logic;
+        mem_load    : in std_logic;
+        id_r1_addr  : in std_logic_vector(4 downto 0);
+        id_r2_addr  : in std_logic_vector(4 downto 0);
+        mem_r1_addr : in std_logic_vector(4 downto 0);
+        --mem_r2_addr : in std_logic_vector(4 downto 0);
+        id_cmp      : in std_logic;
+        mem_cmp     : in std_logic;
+        ex_cmp      : in std_logic;
+        no_op       : out std_logic;
+        stall_op    : out std_logic;
+    
+    );
 end hazard_dectection_unit;
 
 architecture Behavioral of hazard_dectection_unit is
 
 begin
-
+    detect_hazard : process(clk) 
+    begin
+        if falling_edge(clk) then
+            if (mem_branch = '1' or ex_branch = '1') then
+                no_op <= '1';
+            elsif (mem_load = '1')
+            and ((id_r1_addr = mem_r1_addr) or (id_r2_addr = mem_r1_addr)) then
+                no_op <= '1';
+    end process;
 
 end Behavioral;
 
