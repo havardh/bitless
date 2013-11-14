1aa8c63bc07f030cf28b89e6ea7c1ed3ba764e3c
x10an14 <x10an14@hotmail.com>
2013-11-14 16:16:00 +0100

    Updated tb_core.chd file for new core.

--- vhdl/testbenches/tb_core.vhd
+++ vhdl/testbenches/tb_core.vhd
@@ -1,90 +1,88 @@
 --------------------------------------------------------------------------------
--- Company:
+-- Company: 
 -- Engineer:
 --
--- Create Date:	16:36:56 11/13/2013
--- Design Name:
--- Module Name:	M:/Github/tdt4295/vhdl/testbenches/tb_core.vhd
+-- Create Date:   15:42:47 11/14/2013
+-- Design Name:   
+-- Module Name:   M:/Github/tdt4295/vhdl/testbenches/tb_core.vhd
 -- Project Name:  tdt4295_core
--- Target Device:
--- Tool versions:
--- Description:
---
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
 -- VHDL Test Bench Created by ISE for module: core
---
+-- 
 -- Dependencies:
---
+-- 
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
 --
--- Notes:
+-- Notes: 
 -- This testbench has been automatically generated using types std_logic and
 -- std_logic_vector for the ports of the unit under test.  Xilinx recommends
 -- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation
+-- to guarantee that the testbench will bind correctly to the post-implementation 
 -- simulation model.
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-
+ 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
-
+ 
 ENTITY tb_core IS
 END tb_core;
+ 
+ARCHITECTURE behavior OF tb_core IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT core
+    PORT(
+         clk : IN  std_logic;
+         pl_stop_core : IN  std_logic;
+         reset : IN  std_logic;
+         proc_finished : OUT  std_logic;
+         instruction_addr : OUT  std_logic_vector(15 downto 0);
+         instruction_data : IN  std_logic_vector(15 downto 0);
+         constant_addr : OUT  std_logic_vector(15 downto 0);
+         constant_data : IN  std_logic_vector(31 downto 0);
+         input_read_addr : OUT  std_logic_vector(15 downto 0);
+         input_read_data : IN  std_logic_vector(31 downto 0);
+         output_write_addr : OUT  std_logic_vector(15 downto 0);
+         output_write_data : OUT  std_logic_vector(31 downto 0);
+         output_we : OUT  std_logic;
+         output_read_addr : OUT  std_logic_vector(15 downto 0);
+         output_read_data : IN  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
 
-ARCHITECTURE behavior OF tb_core IS
-
-	 -- Component Declaration for the Unit Under Test (UUT)
-	 COMPONENT core
-	 PORT(
-			clk : IN  std_logic;
-			memclk : IN  std_logic;
-			sample_clk : IN  std_logic;
-			reset : IN  std_logic;
-			proc_finished : OUT  std_logic;
-			instruction_addr : OUT  std_logic_vector(15 downto 0);
-			instruction_data : IN  std_logic_vector(15 downto 0);
-			constant_addr : OUT  std_logic_vector(15 downto 0);
-			constant_data : IN  std_logic_vector(31 downto 0);
-			input_read_addr : OUT  std_logic_vector(15 downto 0);
-			input_read_data : IN  std_logic_vector(31 downto 0);
-			output_write_addr : OUT  std_logic_vector(15 downto 0);
-			output_write_data : OUT  std_logic_vector(31 downto 0);
-			output_we : OUT  std_logic;
-			output_read_addr : OUT  std_logic_vector(15 downto 0);
-			output_read_data : IN  std_logic_vector(31 downto 0)
-		  );
-	 END COMPONENT;
-
-	--Inputs
-	signal clk : std_logic := '0';
-	signal memclk : std_logic := '0';
-	signal sample_clk : std_logic := '0';
-	signal reset : std_logic := '0';
-	signal instruction_data : std_logic_vector(15 downto 0) := (others => '0');
-	signal constant_data : std_logic_vector(31 downto 0) := (others => '0');
-	signal input_read_data : std_logic_vector(31 downto 0) := (others => '0');
-	signal output_read_data : std_logic_vector(31 downto 0) := (others => '0');
+   --Inputs
+   signal clk : std_logic := '0';
+   signal pl_stop_core : std_logic := '0';
+   signal reset : std_logic := '0';
+   signal instruction_data : std_logic_vector(15 downto 0) := (others => '0');
+   signal constant_data : std_logic_vector(31 downto 0) := (others => '0');
+   signal input_read_data : std_logic_vector(31 downto 0) := (others => '0');
+   signal output_read_data : std_logic_vector(31 downto 0) := (others => '0');
 
  	--Outputs
-	signal proc_finished : std_logic;
-	signal instruction_addr : std_logic_vector(15 downto 0);
-	signal constant_addr : std_logic_vector(15 downto 0);
-	signal input_read_addr : std_logic_vector(15 downto 0);
-	signal output_write_addr : std_logic_vector(15 downto 0);
-	signal output_write_data : std_logic_vector(31 downto 0);
-	signal output_we : std_logic;
-	signal output_read_addr : std_logic_vector(15 downto 0);
+   signal proc_finished : std_logic;
+   signal instruction_addr : std_logic_vector(15 downto 0);
+   signal constant_addr : std_logic_vector(15 downto 0);
+   signal input_read_addr : std_logic_vector(15 downto 0);
+   signal output_write_addr : std_logic_vector(15 downto 0);
+   signal output_write_data : std_logic_vector(31 downto 0);
+   signal output_we : std_logic;
+   signal output_read_addr : std_logic_vector(15 downto 0);
 
-	-- Clock period definitions
-	constant clk_period : time := 10 ns;
-	constant memclk_period : time := 10 ns;
-	constant sample_clk_period : time := 10 ns;
-
-	--Signal constants
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+   
+   --Signal constants
 	constant zero32b : std_logic_vector(31 downto 0) := X"00000000";
 	constant wait_period : integer := 5; --Should be the clockcycles an instruction needs to propagate through the whole pipelined core
 
@@ -123,67 +121,50 @@ ARCHITECTURE behavior OF tb_core IS
 	constant load_inpt_6 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load3 &"00111"&"00000"; --Load the value pointed to by the address in register 0 to register 7. Value loads to constant buffer
 	constant load_inpt_7 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load3 &"01000"&"00000"; --Load the value pointed to by the address in register 0 to register 8. Value loads to constant buffer
 	constant load_inpt_8 : std_logic_vector(15 downto 0) := gc_load_store & fc_load_store & oc_load3 &"01001"&"00000"; --Load the value pointed to by the address in register 0 to register 9. Value loads to constant buffer
-
+ 
 BEGIN
-
+ 
 	-- Instantiate the Unit Under Test (UUT)
-	uut: core PORT MAP (
-			 clk => clk,
-			 memclk => memclk,
-			 sample_clk => sample_clk,
-			 reset => reset,
-			 proc_finished => proc_finished,
-			 instruction_addr => instruction_addr,
-			 instruction_data => instruction_data,
-			 constant_addr => constant_addr,
-			 constant_data => constant_data,
-			 input_read_addr => input_read_addr,
-			 input_read_data => input_read_data,
-			 output_write_addr => output_write_addr,
-			 output_write_data => output_write_data,
-			 output_we => output_we,
-			 output_read_addr => output_read_addr,
-			 output_read_data => output_read_data
-		  );
+   uut: core PORT MAP (
+          clk => clk,
+          pl_stop_core => pl_stop_core,
+          reset => reset,
+          proc_finished => proc_finished,
+          instruction_addr => instruction_addr,
+          instruction_data => instruction_data,
+          constant_addr => constant_addr,
+          constant_data => constant_data,
+          input_read_addr => input_read_addr,
+          input_read_data => input_read_data,
+          output_write_addr => output_write_addr,
+          output_write_data => output_write_data,
+          output_we => output_we,
+          output_read_addr => output_read_addr,
+          output_read_data => output_read_data
+        );
 
-	-- Clock process definitions
-	clk_process :process
-	begin
+   -- Clock process definitions
+   clk_process :process
+   begin
 		clk <= '0';
 		wait for clk_period/2;
 		clk <= '1';
 		wait for clk_period/2;
-	end process;
+   end process;
+ 
 
-	memclk_process :process
-	begin
-		memclk <= '0';
-		wait for memclk_period/2;
-		memclk <= '1';
-		wait for memclk_period/2;
-	end process;
-
-	sample_clk_process :process
-	begin
-		sample_clk <= '0';
-		wait for sample_clk_period/2;
-		sample_clk <= '1';
-		wait for sample_clk_period/2;
-	end process;
-
-
-	-- Stimulus process
-	stim_proc: process
-	begin
-		-- hold reset state for 100 ns.
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
 	  reset <= '1';
-		wait for 100 ns;
+      wait for 100 ns;
 	  reset <= '0';
 
-		wait for clk_period*10;
+      wait for clk_period*10;
 
-		-- insert stimulus here
-
+      -- insert stimulus here
+	  
 --		--When commands are run, you need one clockcycle between each command, like shown below
 --		--First instruction
 --		instruction_data <= instruction_1;
@@ -272,7 +253,7 @@ BEGIN
 		--Check if register 9 has the above hex value, by checking the value on signal output_write_data
 		instruction_data <= store_outpt_8;
 
-		wait;
-	end process;
+      wait;
+   end process;
 
 END;
