3cddc87a6277967b775d024dd5138d87cc21a67f
Olav Emil Eiksund <olavemil@stud.ntnu.no>
2013-11-14 12:20:16 +0100

    Fixed fpu bug

--- vhdl/pipeline/core/alu/fpu.vhd
+++ vhdl/pipeline/core/alu/fpu.vhd
@@ -7,7 +7,7 @@ use work.core_constants.all;
 
 entity fpu is
 	port (
-		a, b, c, d			: in	std_logic_vector(15 downto 0);
+		a, b, const_c, const_d			: in	std_logic_vector(15 downto 0);
 		aluop_in			: in	alu_operation;
 		cpu_clk, alu_clk 	: in	std_logic;
 		result, result_2	: out	std_logic_vector(15 downto 0);
@@ -16,6 +16,7 @@ entity fpu is
 end fpu;
 
 architecture behaviour of fpu is
+
 	component fp_multiply
 		port(
 			a			: in std_logic_vector(15 downto 0);
@@ -42,6 +43,10 @@ architecture behaviour of fpu is
 	signal fp_addsub_operation : std_logic_vector(5 downto 0);
 	signal fp_addsub_flag_underflow, fp_addsub_flag_overflow : std_logic;
 	
+	--control signals
+--	signal mul_select : std_logic_vector(1 downto 0);
+--	signal add_select : std_logic_vector(1 downto 0);
+	
 begin
 	floating_point_multiplier : fp_multiply
 		port map(
@@ -64,7 +69,7 @@ begin
 	
 	result_2 <= (others => '0');
 	
-	fpu_ctrl : process(aluop_in, a, b, c, d, fp_mul_flag_overflow, fp_addsub_flag_overflow, fp_mul_result_out, fp_addsub_result_out)
+	fpu_ctrl : process(aluop_in, a, b, const_c, const_d, fp_mul_flag_overflow, fp_addsub_flag_overflow, fp_mul_result_out, fp_addsub_result_out)
 	begin
 		case aluop_in is
 			when fp_mul =>--FLOATING POINT MULTIPLICATION
@@ -102,7 +107,7 @@ begin
 				
 			when fp_mac =>--FLOATING POINT MULTIPLY AND ACCUMULATE B += A*C
 				fp_mul_data_in_1 <= a;
-				fp_mul_data_in_2 <= c;
+				fp_mul_data_in_2 <= const_c;
 				
 				fp_addsub_data_in_1 <= b;
 				fp_addsub_data_in_2 <= fp_mul_result_out;
@@ -110,13 +115,10 @@ begin
 				
 				flags <= ('0', '0', fp_mul_flag_overflow or fp_addsub_flag_overflow, '0');
 				result <= fp_addsub_result_out;
-				--
-				fp_mul_data_in_1 <= (others => '0');
-				fp_mul_data_in_2 <= (others => '0');
 				
 			when fp_mad =>--FLOATING POINT MULTIPLY AND ACCUMULATE A += B*D
-				fp_mul_data_in_1 <= b;
-				fp_mul_data_in_2 <= d;
+				fp_mul_data_in_1 <= const_d;
+				fp_mul_data_in_2 <= b;
 				
 				fp_addsub_data_in_1 <= a;
 				fp_addsub_data_in_2 <= fp_mul_result_out;
@@ -124,13 +126,10 @@ begin
 				
 				flags <= ('0', '0', fp_mul_flag_overflow or fp_addsub_flag_overflow, '0');
 				result <= fp_addsub_result_out;
-				--
-				fp_mul_data_in_1 <= (others => '0');
-				fp_mul_data_in_2 <= (others => '0');
 				
 			when fp_msc =>--FLOATING POINT MULTIPLY AND SUBTRACT B -= A*C
 				fp_mul_data_in_1 <= a;
-				fp_mul_data_in_2 <= c;
+				fp_mul_data_in_2 <= const_c;
 				
 				fp_addsub_data_in_1 <= b;
 				fp_addsub_data_in_2 <= fp_mul_result_out;
@@ -138,13 +137,10 @@ begin
 				
 				flags <= ('0', '0', fp_mul_flag_overflow or fp_addsub_flag_overflow, '0');
 				result <= fp_addsub_result_out;
-				--
-				fp_mul_data_in_1 <= (others => '0');
-				fp_mul_data_in_2 <= (others => '0');
 				
 			when fp_msd =>--FLOATING POINT MULTIPLY AND SUBTRACT A -= B*D
-				fp_mul_data_in_1 <= b;
-				fp_mul_data_in_2 <= d;
+				fp_mul_data_in_1 <= const_d;
+				fp_mul_data_in_2 <= b;
 				
 				fp_addsub_data_in_1 <= a;
 				fp_addsub_data_in_2 <= fp_mul_result_out;
@@ -152,20 +148,47 @@ begin
 				
 				flags <= ('0', '0', fp_mul_flag_overflow or fp_addsub_flag_overflow, '0');
 				result <= fp_addsub_result_out;
-				--
-				fp_mul_data_in_1 <= (others => '0');
-				fp_mul_data_in_2 <= (others => '0');
 				
 			when others =>--NOT FLOATING POINT OPERATION. BORING...
 				--
-				fp_mul_data_in_1 <= (others => '0');
-				fp_mul_data_in_2 <= (others => '0');
-				fp_addsub_data_in_1 <= (others => '0');
-				fp_addsub_data_in_2 <= (others => '0');
+				fp_mul_data_in_1 <= a;--(others => '0');
+				fp_mul_data_in_2 <= b;--(others => '0');
+				fp_addsub_data_in_1 <= a;--(others => '0');
+				fp_addsub_data_in_2 <= b;--(others => '0');
 				fp_addsub_operation <= "000000";
 				
 				flags <= ('0', '0', '0', '0');
-				result <= (others => '0');
+				result <= a;--(others => '0');
 		end case;
 	end process;
+	
+--	mux_mul_select : process(mul_select)
+--	begin
+--		if mul_select(0) = '0' then
+--			fp_mul_data_in_1	<= a;
+--		else
+--			fp_mul_data_in_1	<= const_d;
+--		end if;
+--		
+--		if mul_select(1) = '0' then
+--			fp_mul_data_in_2	<= b;
+--		else
+--			fp_mul_data_in_2	<= const_c;
+--		end if;
+--	end process;
+--	
+--	mux_add_select : process(add_select)
+--	begin
+--		if add_select(0) = '0' then
+--			fp_addsub_data_in_1	<= a;
+--		else
+--			fp_addsub_data_in_1	<= fp_mul_result_out;
+--		end if;
+--		
+--		if add_select(1) = '0' then
+--			fp_addsub_data_in_2	<= b;
+--		else
+--			fp_addsub_data_in_2	<= fp_mul_result_out;
+--		end if;
+--	end process;
 end behaviour;
