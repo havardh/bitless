1ee1442aca8eaf40cad1f6a7586160fddcc9c5d1
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-11-15 16:50:40 +0100

    Added two core test program

--- sw/modules/ebi/demo/fpga.c
+++ sw/modules/ebi/demo/fpga.c
@@ -15,6 +15,8 @@
 #define DATA_LENGTH     2
 #define BIT_HIGH(var,pos) ((var) & (1<<(pos)))
 
+#define fpga_address 0x80000000
+
 void read(volatile uint16_t *addr, volatile uint16_t *data) {
 
 	*data = *addr;
@@ -27,51 +29,103 @@ void write_data(volatile uint16_t *addr, volatile uint16_t data) {
 
 void load_store(void) 
 {
-	write_data(fpga_address + 0x44000, 0x0000);
-	write_data(fpga_address + 0x44001, 0x7080);
-	write_data(fpga_address + 0x44002, 0x0000);
-	write_data(fpga_address + 0x44003, 0x0000);
-	write_data(fpga_address + 0x44004, 0x0000);
-	write_data(fpga_address + 0x44005, 0x0000);
-	write_data(fpga_address + 0x44006, 0x7c80);
-	write_data(fpga_address + 0x44007, 0x0000);
-	write_data(fpga_address + 0x44008, 0x0000);
-	write_data(fpga_address + 0x44009, 0x0000);
-	write_data(fpga_address + 0x4400a, 0x3000);
-	write_data(fpga_address + 0x20000, 0x7fff);
-	write_data(fpga_address + 0x40000, 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44000), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44001), 0x7080);
+	write_data((uint16_t*)(fpga_address + 0x44002), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44003), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44004), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44005), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44006), 0x7c80);
+	write_data((uint16_t*)(fpga_address + 0x44007), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44008), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44009), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x4400a), 0x3000);
+	write_data((uint16_t*)(fpga_address + 0x20000), 0x7fff);
+	write_data((uint16_t*)(fpga_address + 0x40000), 0x0000);
 	
-	write_data(fpga_address + 0x40000, 0x0002);
+	write_data((uint16_t*)(fpga_address + 0x40000), 0x0002);
 	
 	uint16_t res;
-	read(fpga_address + 0x30000, &res);
+	read((uint16_t*)(fpga_address + 0x30000), &res);
 	
-	write_data(fpga_address + 0x40000, 0x0002);
+	write_data((uint16_t*)(fpga_address + 0x40000), 0x0002);
 
+}
+
+void load_store2cores(void) 
+{
+	//Skriv program til kjerne 0:
+	write_data((uint16_t*)(fpga_address + 0x44000), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44001), 0x7080);
+	write_data((uint16_t*)(fpga_address + 0x44002), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44003), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44004), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44005), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44006), 0x7c80);
+	write_data((uint16_t*)(fpga_address + 0x44007), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44008), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44009), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x4400a), 0x3000);
+
+ 	//Skriv program til kjerne 1:
+	write_data((uint16_t*)(fpga_address + 0x54000), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x54001), 0x7080);
+	write_data((uint16_t*)(fpga_address + 0x54002), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x54003), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x54004), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x54005), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x54006), 0x7c80);
+	write_data((uint16_t*)(fpga_address + 0x54007), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x54008), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x54009), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x5400a), 0x3000);
+
+	//Skriv data til input-buffer:
+	write_data((uint16_t*)(fpga_address + 0x20000), 0xbeef);
+
+	//Start kjerne 0:
+	write_data((uint16_t*)(fpga_address + 0x40000), 0x0000);
+	//Start kjerne 1:
+	write_data((uint16_t*)(fpga_address + 0x50000), 0x0000);
+
+	// Vent litt), send én puls på sample_clk), vent litt til.
+	
+	GPIO_PinOutToggle(gpioPortF, 12);
+
+	
+	//Stopp kjerne 0:
+	write_data((uint16_t*)(fpga_address + 0x40000), 0x0002);
+	//Stopp kjerne 1:
+	write_data((uint16_t*)(fpga_address + 0x50000), 0x0002);
+
+	uint16_t res;
+	read((uint16_t*)(fpga_address + 0x30000), &res);
+
+	write_data((uint16_t*)(fpga_address + 0x40000), 0x0002);
 }
 
 void add1(void) 
 {
-	write_data(fpga_address + 0x44000, 0x0000);
-	write_data(fpga_address + 0x44001, 0x7080);
-	write_data(fpga_address + 0x44002, 0x0000);
-	write_data(fpga_address + 0x44003, 0x0000);
-	write_data(fpga_address + 0x44004, 0x0000);
-	write_data(fpga_address + 0x44005, 0x0000);
-	write_data(fpga_address + 0x44006, 0x7c80);
-	write_data(fpga_address + 0x44007, 0x0000);
-	write_data(fpga_address + 0x44008, 0x0000);
-	write_data(fpga_address + 0x44009, 0x0000);
-	write_data(fpga_address + 0x4400a, 0x3000);
-	write_data(fpga_address + 0x20000, 0x7fff);
-	write_data(fpga_address + 0x40000, 0x0000);
+
+
+	write_data((uint16_t*)(fpga_address + 0x44000), 0x7040);
+	write_data((uint16_t*)(fpga_address + 0x44001), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44002), 0x8001);
+	write_data((uint16_t*)(fpga_address + 0x44003), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44004), 0x0041);
+	write_data((uint16_t*)(fpga_address + 0x44005), 0x7c40);
+	write_data((uint16_t*)(fpga_address + 0x44006), 0x0000);
+	write_data((uint16_t*)(fpga_address + 0x44007), 0x3000);
+
+	write_data((uint16_t*)(fpga_address + 0x20000), 0x7fff);
+	write_data((uint16_t*)(fpga_address + 0x40000), 0x0000);
 	
-	write_data(fpga_address + 0x40000, 0x0002);
+	write_data((uint16_t*)(fpga_address + 0x40000), 0x0002);
 	
 	uint16_t res;
-	read(fpga_address + 0x30000, &res);
+	read((uint16_t*)(fpga_address + 0x30000), &res);
 	
-	write_data(fpga_address + 0x40000, 0x0002);
+	write_data((uint16_t*)(fpga_address + 0x40000), 0x0002);
 
 }
 
@@ -80,8 +134,10 @@ void add1(void)
 int main(void) {
 	CHIP_Init();
 	EBIDriver_Init();
+	CMU_ClockEnable(cmuClock_GPIO, true);
 
-	volatile uint16_t *fpga_address = (uint16_t*) 0x80000000;
+	GPIO_PinModeSet(gpioPortF,  12, gpioModePushPull, 0);
+	GPIO_PinOutClear(gpioPortF, 12);
 	// uint32_t addr0 = 0x4000;
 	// uint32_t addr1 = 0x40000;
 	// uint32_t toplevel_address = 0x400000;
@@ -90,6 +146,7 @@ int main(void) {
 	
 	
 	while (1) {
+		load_store2cores();
 		// write_data(fpga_address, 1, 0);
 		//read((fpga_address), 1, fpga_res);
 		// read((fpga_address + number), 1, fpga_res);
